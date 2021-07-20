532ec1ab291ed076e08b1ab1621ef1518236a44b
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-07-20 03:04:19 +0200

    complete episode 1

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("xd")
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		// log.Println("hello world")
+		d, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			rw.WriteHeader(http.StatusBadRequest)
+			rw.Write([]byte("Bad Request"))
+		}
+		fmt.Fprintf(rw, "Hello %s", d)
+	})
+
+	http.HandleFunc("/goodbye", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Goodbye World")
+		log.Println(r.Body)
+
+	})
+
+	http.ListenAndServe(":9090", nil)
 }
